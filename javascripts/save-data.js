bc37778739e29b13923bdfa0f77e9cb7ff45231a
danielcossins11 <daniel.cossins@yahoo.com>
2015-08-19 21:12:24 -0500

    got visited working plus other clean-ups

--- javascripts/save-data.js
+++ javascripts/save-data.js
@@ -1,16 +1,32 @@
 define(function(require){
-  var $= require("jquery");
+  var $ = require("jquery");
+
+  var newLocation = {
+    location: "Moon",
+    location_type: "Out-of-this World",
+    reviews: {
+      date: 0,
+      title: "",
+      text: ""
+    },
+    visited: false
+  };
+
+  $("#visited").click(function() {
+    newLocation.visited = true;
+  });
+
+  $("#wish-list").click(function() {
+    newLocation.visited = false;
+  });
+
   $("#submit").on("click", function(){
-    var newLocation = {
-      location: $('#place').val(),
-      location_type: $('#type').val(),
-      reviews: {
-        date: $('#reviewDate').val(),
-        title: $('#reviewTitle').val(),
-        text: $('#reviewText').val(),
-      },
-      visited: $('#false').val()
-    };
+    newLocation.location = $('#place').val();
+    newLocation.location_type = $('#type').val();
+    newLocation.reviews.date = $('#reviewDate').val();
+    newLocation.reviews.title = $('#reviewTitle').val();
+    newLocation.reviews.text = $('#reviewText').val();
+
 
     $.ajax({
       url: "https://nss-trippin.firebaseio.com/trips.json",
